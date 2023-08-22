5d5c7e7232bf28045ebb8409bd42ba2ad275ae77
Svarlione <ruslan.brigockij@gmail.com>
2023-08-22 21:06:57 +0300

    Add project files.

---
+++ uzduotis7/Program.cs
@@ -0,0 +1,6 @@
+﻿string word = "Kolumbija";
+int wordLength = word.Length;
+char letter = word[3];
+Console.WriteLine("Miestas: "+ word);
+Console.WriteLine("Zodzio ilgis : " + wordLength);
+Console.WriteLine("Raide numeris 3: " + letter);
